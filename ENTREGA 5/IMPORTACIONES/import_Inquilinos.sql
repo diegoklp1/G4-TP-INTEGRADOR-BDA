2bc89f6966757187ea26411a8d686ab2c29b4a8c
diegoklp1 <llanosdiegowk@gmail.com>
2025-10-31 21:28:31 -0300

    Importaciones (4 de 7)

--- ENTREGA 5/IMPORTACIONES/import_Inquilinos.sql
+++ ENTREGA 5/IMPORTACIONES/import_Inquilinos.sql
@@ -59,26 +59,14 @@ BEGIN
             DNI_Limpio = LTRIM(RTRIM(DNI_CSV)),
             Email_CSV = LOWER(LTRIM(RTRIM(Email_CSV))),
             Telefono_CSV = LTRIM(RTRIM(Telefono_CSV)),
-            CBU_CSV = CASE 
-                -- Si CBU está en notación científica (3,62...E+021)
-                WHEN LTRIM(RTRIM(CBU_CSV)) LIKE '%E+%' THEN 
-                    CONVERT(VARCHAR(30), --4 Convierto el número a texto
-                        CAST( -- 3 Convierto a DECIMAL para expandir el número
-                            CAST( -- 2 Convierto a FLOAT para tomar el 'E+21'
-                                REPLACE(LTRIM(RTRIM(CBU_CSV)), ',', '.') AS FLOAT -- 1 Reemplazo , por . 
-                            ) 
-                            AS DECIMAL(28, 0) 
-                        )
-                    )
-                -- Si no es notación científica, solo se limpia
-                ELSE LTRIM(RTRIM(CBU_CSV))
-            END;
+            CBU_CSV = LTRIM(RTRIM(CBU_CSV));
 
        
         UPDATE #TempPersonas SET Es_Inquilino_BIT = CASE WHEN LTRIM(RTRIM(Inquilino_CSV)) = '1' THEN 1 ELSE 0 END;
         UPDATE #TempPersonas SET DNI_Limpio = NULL WHERE ISNUMERIC(DNI_Limpio) = 0;
         DELETE FROM #TempPersonas WHERE DNI_Limpio IS NULL;
 
+		/*
 		-- VALIDACIÓN DE DUPLICADOS EN ORIGEN
 		DECLARE @DNIDuplicado VARCHAR(20);
 		SELECT TOP 1 @DNIDuplicado = DNI_Limpio
@@ -92,25 +80,26 @@ BEGIN
         DECLARE @ErrorMsg VARCHAR(200) = 'ERROR: El archivo CSV tiene DNIs duplicados. El DNI ' + @DNIDuplicado + ' aparece más de una vez. Corregir archivo de origen.'; 
         THROW 50001, @ErrorMsg, 1;
         END
+		*/
 
         -- MERGE
         MERGE INTO Persona AS T -- Target
         USING (
-            -- DISTINCT para valores únicos
-            SELECT DISTINCT
+            SELECT 
                 DNI_Limpio,
                 Nombre_CSV,
                 Apellido_CSV,
                 Email_CSV,
                 Telefono_CSV,
                 CBU_CSV,
-                Es_Inquilino_BIT
+                Es_Inquilino_BIT,
+				ROW_NUMBER() OVER(PARTITION BY DNI_Limpio ORDER BY DNI_Limpio) AS rn
             FROM #TempPersonas
         ) AS S -- Source
-        ON (T.DNI = S.DNI_Limpio)
+        ON (T.DNI = S.DNI_Limpio AND S.rn = 1)
 
         -- Si el DNI no existe, inserta la persona
-        WHEN NOT MATCHED BY TARGET THEN
+        WHEN NOT MATCHED BY TARGET AND s.rn = 1 THEN
             INSERT (
                 -- Id_persona (PK) se asume IDENTITY
                 DNI,
@@ -153,7 +142,10 @@ END
 GO
 
 EXEC sp_Importar_Personas
-    @RutaArchivoCSV = 'D:\Diego\Downloads\Archivos para el TP\Inquilino-propietarios-datos.csv';
+    @RutaArchivoCSV = 'D:\Diego\Downloads\ARCHIVOS_TP_BDA\Inquilino-propietarios-datos.csv';
 GO
 
-SELECT * FROM Persona
+
+truncate table persona
+use bd_tp_testeo
+select * from dbo.Persona
