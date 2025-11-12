d7c5c6b111abae664858d5802fdd09a6adb3e2d5
danvare1 <davarela@alumno.unlam.edu.ar>
2025-11-12 20:42:49 -0300

    Indices

--- ENTREGA 6/INDICES/Indices.sql
+++ ENTREGA 6/INDICES/Indices.sql
@@ -1,73 +1,27 @@
---FALTA - CONTINUAR
---Administración / Consorcio
-CREATE INDEX IX_Consorcio_IdAdministracion
-    ON Consorcio(Id_Administracion);
+---Reporte 1 y 2
+CREATE NONCLUSTERED INDEX IX_Pago ON Pago (
+Id_Pago, Fecha);
+CREATE NONCLUSTERED INDEX IX_Detalle_Pago ON Detalle_Pago (
+Id_Pago, Id_Detalle_Expensa, Importe_Usado, Id_Tipo_Ingreso);
+CREATE NONCLUSTERED INDEX IX_Detalle_Expensa_UF ON Detalle_Expensa_UF (
+Id_Detalle_Expensa, Id_Expensa, Id_Consorcio, NroUF, Importe_Ordinario_Prorrateado, Importe_Extraordinario_Prorrateado);
+CREATE NONCLUSTERED INDEX IX_Liquidacion_Mensual ON Liquidacion_Mensual (
+Id_Liquidacion_Mensual, Id_Consorcio, Fecha_Vencimiento1, Periodo);
+--Reporte 2
+CREATE NONCLUSTERED INDEX IX_Unidad_Funcional ON Unidad_Funcional (
+Id_Consorcio, Piso, NroUF, Departamento);
 
---Proveedores
-CREATE INDEX IX_Proovedor_IdConsorcio
-    ON Proovedor(Id_Consorcio);
+--Reporte 3
+CREATE NONCLUSTERED INDEX IX_Tipo_Ingreso ON Tipo_Ingreso (
+Id_Tipo_Ingreso, Nombre);
 
---Estado Financiero
-CREATE INDEX IX_EstadoFinanciero_IdConsorcio
-    ON Estado_Financiero(Id_Consorcio);
+--Reporte 4
+CREATE NONCLUSTERED INDEX IX_Gasto_Ordinario ON Gasto_Ordinario (
+Id_Consorcio, Id_Tipo_Gasto, Fecha);
+CREATE NONCLUSTERED INDEX IX_Gasto_Extraordinario ON Gasto_Extraordinario (
+Id_Consorcio, Fecha);
 
-CREATE INDEX IX_EstadoFinanciero_Periodo
-    ON Estado_Financiero(Periodo);
-
---Gastos Ordinarios
-CREATE INDEX IX_GastoOrdinario_IdConsorcio
-    ON Gasto_Ordinario(Id_Consorcio);
-
-CREATE INDEX IX_GastoOrdinario_IdTipoGasto
-    ON Gasto_Ordinario(Id_Tipo_Gasto);
-
-CREATE INDEX IX_GastoOrdinario_Fecha
-    ON Gasto_Ordinario(Fecha);
-
---Gastos Extraordinarios
-CREATE INDEX IX_GastoExtra_IdConsorcio
-    ON Gasto_Extraordinario(Id_Consorcio);
-
-CREATE INDEX IX_GastoExtra_IdTipoPago
-    ON Gasto_Extraordinario(Id_tipo_pago);
-
-CREATE INDEX IX_GastoExtra_Fecha
-    ON Gasto_Extraordinario(Fecha);
-
---Unidad Funcional / Personas / Relaciones
--- Consultas por consorcio
-CREATE INDEX IX_UF_IdConsorcio
-    ON Unidad_Funcional(Id_Consorcio);
-
-CREATE INDEX IX_UnidadPersona_IdConsorcio
-    ON Unidad_Persona(Id_Consorcio);
-
-CREATE INDEX IX_UnidadPersona_IdPersona
-    ON Unidad_Persona(Id_Persona);
-
-CREATE INDEX IX_UnidadPersona_IdTipoRelacion
-    ON Unidad_Persona(Id_TipoRelacion);
-
---Liquidación / Expensa UF
-CREATE INDEX IX_LiquidacionMensual_IdConsorcio
-    ON Liquidacion_Mensual(Id_Consorcio);
-
-CREATE INDEX IX_LiquidacionMensual_Periodo
-    ON Liquidacion_Mensual(Periodo);
-
---Pagos / Detalle de Pago
-CREATE INDEX IX_Pago_IdFormaPago
-    ON Pago(Id_Forma_De_Pago);
-
-CREATE INDEX IX_Pago_Fecha
-    ON Pago(Fecha);
-
-CREATE INDEX IX_DetallePago_IdPago
-    ON Detalle_Pago(Id_Pago);
-
-CREATE INDEX IX_DetallePago_IdDetalleExpensa
-    ON Detalle_Pago(Id_Detalle_Expensa);
-
-CREATE INDEX IX_DetallePago_IdTipoIngreso
-    ON Detalle_Pago(Id_Tipo_Ingreso);
-
+--Reporte 5
+CREATE NONCLUSTERED INDEX IX_Consorcio ON Consorcio (Id_Consorcio);
+CREATE NONCLUSTERED INDEX IX_Unidad_Persona ON Consorcio (Id_Consorcio, Id_Persona, NroUF);
+CREATE NONCLUSTERED INDEX IX_Persona ON Persona (Id_Persona, NroUF);
