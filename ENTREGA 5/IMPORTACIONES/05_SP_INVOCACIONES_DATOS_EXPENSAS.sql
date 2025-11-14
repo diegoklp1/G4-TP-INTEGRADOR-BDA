95cefc23e0b03a2f5ca763e334d9a9a087d7b289
francoll12 <57900857+francoll12@users.noreply.github.com>
2025-11-14 13:48:21 -0300

    Update 05_SP_INVOCACIONES_DATOS_EXPENSAS.sql

--- ENTREGA 5/IMPORTACIONES/05_SP_INVOCACIONES_DATOS_EXPENSAS.sql
+++ ENTREGA 5/IMPORTACIONES/05_SP_INVOCACIONES_DATOS_EXPENSAS.sql
@@ -9,20 +9,18 @@
 -- Integrantes:
 -- - Llanos Franco , DNI: 43629080
 -- - Varela Daniel , DNI: 40388978
--- - Llanos Diego  , DNI: 45748387
-
+-- - Llanos Diego  , DNI: 45748387
 -- =============================================================
 
 USE COM5600_G04;
 GO
-
 IF NOT EXISTS (SELECT 1 FROM SYS.COLUMNS 
-               WHERE OBJECT_ID = OBJECT_ID('Pago') 
-               AND NAME = 'Procesado')
+ 				WHERE OBJECT_ID = OBJECT_ID('pagos.Pago') 
+ 				AND NAME = 'Procesado')
 BEGIN
-    ALTER TABLE Pago
-    ADD Procesado BIT NOT NULL DEFAULT 0;
-    PRINT 'Columna [Procesado] agregada a la tabla Pago.';
+	ALTER TABLE pagos.Pago
+	ADD Procesado BIT NOT NULL DEFAULT 0;
+	PRINT 'Columna [Procesado] agregada a la tabla pagos.Pago.';
 END
 GO
 USE COM5600_G04
@@ -30,10 +28,10 @@ GO
 
 -- 1. LIMPIEZA (Solo para pruebas)
 PRINT 'Limpiando tablas...';
-DELETE FROM Detalle_Pago;
-DELETE FROM Pago;
-DELETE FROM Detalle_Expensa_UF;
-DELETE FROM Liquidacion_Mensual;
+DELETE FROM pagos.Detalle_Pago;
+DELETE FROM pagos.Pago;
+DELETE FROM liquidacion.Detalle_Expensa_UF;
+DELETE FROM liquidacion.Liquidacion_Mensual;
 GO
 USE COM5600_G04;
 GO
@@ -43,10 +41,10 @@ GO
 -- GASTO EXTRAORDINARIO - Abril 2025
 ----------------------------------------------------------------------
 GO
-INSERT INTO Gasto_Extraordinario 
-    (Id_Consorcio, Id_tipo_pago, detalle_trabajo, Nro_Cuotas_Actual, Total_Cuotas, Importe, Fecha)
+INSERT INTO gastos.Gasto_Extraordinario 
+	(Id_Consorcio, Id_tipo_pago, detalle_trabajo, Nro_Cuotas_Actual, Total_Cuotas, Importe, Fecha)
 VALUES 
-    (1, 1, 'Reparacion Porton Cochera', 1, 1, 45000.00, '2025-04-10');
+	(1, 1, 'Reparacion Porton Cochera', 1, 1, 45000.00, '2025-04-10');
 GO
 ----------------------------------------------------------------------
 -- MES 1 - Abril 2025
@@ -54,37 +52,37 @@ GO
 DECLARE @IdLiquidacionMes1 INT;
 
 SELECT @IdLiquidacionMes1 = Id_Liquidacion_Mensual
-FROM Liquidacion_Mensual
+FROM liquidacion.Liquidacion_Mensual
 WHERE Id_Consorcio = 1
   AND CAST(Periodo AS DATE) = '2025-04-01';
 
 IF @IdLiquidacionMes1 IS NULL
 BEGIN
 
-    PRINT 'Generando liquidación Abril 2025';
+	PRINT 'Generando liquidación Abril 2025';
 
-    --Comentado, descomentar si se busca comparar por una fecha especifica
-    --IF dbo.EsDiaNoHabil('2025-11-24') = 1
-    IF dbo.EsDiaNoHabil(CONVERT(date, GETDATE())) = 1
-    BEGIN
-        EXEC sp_Generar_Liquidacion_Mensual
-            @Id_Consorcio = @IdConsorcioPrueba,
-            @Anio = 2025,
-            @Mes = 4,
-            @Id_Liquidacion_Generada = @IdLiquidacionMes1 OUTPUT;
+	--Comentado, descomentar si se busca comparar por una fecha especifica
+	--IF dbo.EsDiaNoHabil('2025-11-24') = 1
+	IF dbo.EsDiaNoHabil(CONVERT(date, GETDATE())) = 1
+	BEGIN
+		EXEC sp_Generar_Liquidacion_Mensual
+				@Id_Consorcio = 1, -- Se reemplaza variable por ID 1
+				@Anio = 2025,
+				@Mes = 4,
+				@Id_Liquidacion_Generada = @IdLiquidacionMes1 OUTPUT;
 
-        EXEC sp_Generar_Detalle_Expensas 
-            @Id_Liquidacion_Mensual = @IdLiquidacionMes1;
-    END
-    ELSE
-    BEGIN
-        PRINT 'La fecha es día NO hábil. No se ejecuta.';
-    END
+		EXEC sp_Generar_Detalle_Expensas 
+				@Id_Liquidacion_Mensual = @IdLiquidacionMes1;
+	END
+	ELSE
+	BEGIN
+		PRINT 'La fecha es día NO hábil. No se ejecuta.';
+	END
 
 END
 ELSE
 BEGIN
-    PRINT 'La liquidación Abril 2025 ya existe. No se genera nuevamente.';
+	PRINT 'La liquidación Abril 2025 ya existe. No se genera nuevamente.';
 END
 GO
 
@@ -94,21 +92,21 @@ GO
 ----------------------------------------------------------------------
 DECLARE @IdLiquidacionMes1 INT =
 (
-    SELECT Id_Liquidacion_Mensual
-    FROM Liquidacion_Mensual
-    WHERE Id_Consorcio = 1
-      AND CAST(Periodo AS DATE) = '2025-04-01'
+	SELECT Id_Liquidacion_Mensual
+	FROM liquidacion.Liquidacion_Mensual
+	WHERE Id_Consorcio = 1
+	  AND CAST(Periodo AS DATE) = '2025-04-01'
 );
 
 IF @IdLiquidacionMes1 IS NULL
-    RAISERROR('No se encontro la liquidacion del Mes 1.', 16, 1);
+	RAISERROR('No se encontro la liquidacion del Mes 1.', 16, 1);
 ELSE
 BEGIN
-    EXEC sp_Procesar_Pagos;
+	EXEC sp_Procesar_Pagos;
 
-    SELECT Id_Detalle_Expensa, NroUf, Pagos_Recibidos_Mes, Total_A_Pagar
-    FROM Detalle_Expensa_UF
-    WHERE Id_Expensa = @IdLiquidacionMes1;
+	SELECT Id_Detalle_Expensa, NroUf, Pagos_Recibidos_Mes, Total_A_Pagar
+	FROM liquidacion.Detalle_Expensa_UF
+	WHERE Id_Expensa = @IdLiquidacionMes1;
 END
 GO
 
@@ -118,34 +116,35 @@ GO
 ----------------------------------------------------------------------
 DECLARE @IdLiquidacionMes1 INT =
 (
-    SELECT Id_Liquidacion_Mensual
-    FROM Liquidacion_Mensual
-    WHERE Id_Consorcio = 1
-      AND CAST(Periodo AS DATE) = '2025-04-01'
+	SELECT Id_Liquidacion_Mensual
+	FROM liquidacion.Liquidacion_Mensual
+	WHERE Id_Consorcio = 1
+	  AND CAST(Periodo AS DATE) = '2025-04-01'
 );
 
 IF @IdLiquidacionMes1 IS NULL
-    RAISERROR('No se encontro la liquidacion del Mes 1.', 16, 1);
+	RAISERROR('No se encontro la liquidacion del Mes 1.', 16, 1);
 ELSE
 BEGIN
-    IF NOT EXISTS (SELECT 1 FROM Pago WHERE Id_Pago = 99999)
-    BEGIN
-        INSERT INTO Pago (Id_Pago, Id_Forma_De_Pago, Fecha, Cuenta_Origen, Importe, Es_Pago_Asociado, Procesado)
-        VALUES (99991, 1, '2025-04-10', '1112192065530490000000', 500.00, 1, 0);
-        
-        -- Pago que no va a tener un cbu asociado
-        INSERT INTO Pago (Id_Pago, Id_Forma_De_Pago, Fecha, Cuenta_Origen, Importe, Es_Pago_Asociado, Procesado)
-        VALUES (99990, 1, '2025-04-10', '1112192065530490000999', 500.00, 1, 0);
-    END
-    ELSE
+	IF NOT EXISTS (SELECT 1 FROM pagos.Pago WHERE Id_Pago = 99999)
+	BEGIN
+		INSERT INTO pagos.Pago (Id_Pago, Id_Forma_De_Pago, Fecha, Cuenta_Origen, Importe, Es_Pago_Asociado, Procesado)
+		VALUES (99991, 1, '2025-04-10', '1112192065530490000000', 500.00, 1, 0);
+		
+		-- Pago que no va a tener un cbu asociado
+		INSERT INTO pagos.Pago (Id_Pago, Id_Forma_De_Pago, Fecha, Cuenta_Origen, Importe, Es_Pago_Asociado, Procesado)
+		VALUES (99990, 1, '2025-04-10', '1112192065530490000999', 500.00, 1, 0);
+	END
+	ELSE
 
-    EXEC sp_Procesar_Pagos;
+	EXEC sp_Procesar_Pagos;
 
-    /* SELECT Id_Detalle_Expensa, NroUf, Pagos_Recibidos_Mes, Total_A_Pagar
-    FROM Detalle_Expensa_UF
-    WHERE Id_Expensa = @IdLiquidacionMes1
-      AND NroUf = '10';
-    */
+	/* -- Se agrega esquema 'liquidacion'
+	SELECT Id_Detalle_Expensa, NroUf, Pagos_Recibidos_Mes, Total_A_Pagar
+	FROM liquidacion.Detalle_Expensa_UF
+	WHERE Id_Expensa = @IdLiquidacionMes1
+	  AND NroUf = '10';
+	*/
 END
 GO
 
@@ -156,39 +155,39 @@ GO
 DECLARE @IdLiquidacionMes2 INT;
 
 SELECT @IdLiquidacionMes2 = Id_Liquidacion_Mensual
-FROM Liquidacion_Mensual
+FROM liquidacion.Liquidacion_Mensual
 WHERE Id_Consorcio = 1
   AND CAST(Periodo AS DATE) = '2025-05-01';
 
 IF @IdLiquidacionMes2 IS NULL
 BEGIN
-    PRINT 'Generando liquidación Mayo 2025';
+	PRINT 'Generando liquidación Mayo 2025';
 
-    --Comentado, descomentar si se busca comparar por una fecha especifica
-    --IF dbo.EsDiaNoHabil('2025-11-24') = 1
-    IF dbo.EsDiaNoHabil(CONVERT(date, GETDATE())) = 1
-    BEGIN
-        EXEC sp_Generar_Liquidacion_Mensual
-            @Id_Consorcio = 1,
-            @Anio = 2025,
-            @Mes = 5,
-            @Id_Liquidacion_Generada = @IdLiquidacionMes2 OUTPUT;
+	--Comentado, descomentar si se busca comparar por una fecha especifica
+	--IF dbo.EsDiaNoHabil('2025-11-24') = 1
+	IF dbo.EsDiaNoHabil(CONVERT(date, GETDATE())) = 1
+	BEGIN
+		EXEC sp_Generar_Liquidacion_Mensual
+				@Id_Consorcio = 1,
+				@Anio = 2025,
+				@Mes = 5,
+				@Id_Liquidacion_Generada = @IdLiquidacionMes2 OUTPUT;
 
-        EXEC sp_Generar_Detalle_Expensas 
-            @Id_Liquidacion_Mensual = @IdLiquidacionMes2;
-    END
-    ELSE
-    BEGIN
-        PRINT 'La fecha es día NO hábil. No se ejecuta.';
-    END
+		EXEC sp_Generar_Detalle_Expensas 
+				@Id_Liquidacion_Mensual = @IdLiquidacionMes2;
+	END
+	ELSE
+	BEGIN
+		PRINT 'La fecha es día NO hábil. No se ejecuta.';
+	END
 END
 ELSE
 BEGIN
-    PRINT 'La liquidación Mayo 2025 ya existe.';
+	PRINT 'La liquidación Mayo 2025 ya existe.';
 END
 
 SELECT *
-FROM Detalle_Expensa_UF
+FROM liquidacion.Detalle_Expensa_UF
 WHERE Id_Expensa = @IdLiquidacionMes2;
 GO
 
@@ -199,38 +198,38 @@ GO
 DECLARE @IdLiquidacionMes3 INT;
 
 SELECT @IdLiquidacionMes3 = Id_Liquidacion_Mensual
-FROM Liquidacion_Mensual
+FROM liquidacion.Liquidacion_Mensual
 WHERE Id_Consorcio = 1
   AND CAST(Periodo AS DATE) = '2025-06-01';
 
 IF @IdLiquidacionMes3 IS NULL
 BEGIN
-    PRINT 'Generando liquidación Junio 2025';
+	PRINT 'Generando liquidación Junio 2025';
 
-    --Comentado, descomentar si se busca comparar por una fecha especifica
-    --IF dbo.EsDiaNoHabil('2025-11-24') = 1
-    IF dbo.EsDiaNoHabil(CONVERT(date, GETDATE())) = 1
-    BEGIN
-        EXEC sp_Generar_Liquidacion_Mensual
-            @Id_Consorcio = 1,
-            @Anio = 2025,
-            @Mes = 6,
-            @Id_Liquidacion_Generada = @IdLiquidacionMes3 OUTPUT;
+	--Comentado, descomentar si se busca comparar por una fecha especifica
+	--IF dbo.EsDiaNoHabil('2025-11-24') = 1
+	IF dbo.EsDiaNoHabil(CONVERT(date, GETDATE())) = 1
+	BEGIN
+		EXEC sp_Generar_Liquidacion_Mensual
+				@Id_Consorcio = 1,
+				@Anio = 2025,
+				@Mes = 6,
+				@Id_Liquidacion_Generada = @IdLiquidacionMes3 OUTPUT;
 
-        EXEC sp_Generar_Detalle_Expensas 
-            @Id_Liquidacion_Mensual = @IdLiquidacionMes3;
-    END
-    ELSE
-    BEGIN
-        PRINT 'La fecha es día NO hábil. No se ejecuta.';
-    END
+		EXEC sp_Generar_Detalle_Expensas 
+				@Id_Liquidacion_Mensual = @IdLiquidacionMes3;
+	END
+	ELSE
+	BEGIN
+		PRINT 'La fecha es día NO hábil. No se ejecuta.';
+	END
 END
 ELSE
 BEGIN
-    PRINT 'La liquidación Junio 2025 ya existe.';
+	PRINT 'La liquidación Junio 2025 ya existe.';
 END
 
 SELECT *
-FROM Detalle_Expensa_UF
+FROM liquidacion.Detalle_Expensa_UF
 WHERE Id_Expensa = @IdLiquidacionMes3;
 GO
