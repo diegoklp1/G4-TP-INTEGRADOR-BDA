a90f87e11f02efd75042c58cee8881a9607bf22c
francoll12 <57900857+francoll12@users.noreply.github.com>
2025-11-14 13:50:30 -0300

    Refactor import script to use dbo schema

--- ENTREGA 5/IMPORTACIONES/04_SP_INVOCACIONES_IMPORTACIONES.sql
+++ ENTREGA 5/IMPORTACIONES/04_SP_INVOCACIONES_IMPORTACIONES.sql
@@ -16,8 +16,7 @@
 -- Integrantes:
 -- - Llanos Franco , DNI: 43629080
 -- - Varela Daniel , DNI: 40388978
--- - Llanos Diego  , DNI: 45748387
-
+-- - Llanos Diego  , DNI: 45748387
 -- =============================================================
 
 USE COM5600_G04;
@@ -27,48 +26,47 @@ PRINT '--- Iniciando Proceso de Importacion de Datos ---';
 GO
 
 -- 1. Importar Consorcios (Excel)
-PRINT '1. Ejecutando sp_Importar_Consorcios...';
-EXEC sp_Importar_Consorcios 
+PRINT '1. Ejecutando dbo.sp_Importar_Consorcios...';
+EXEC dbo.sp_Importar_Consorcios 
     @RutaArchivoXLSX = 'D:\Diego\Downloads\ARCHIVOS_TP_BDA\ARCHIVOS\datos varios.xlsx',
     @Id_Adm = 1; -- Asumiendo que el ID de la Admin es 1
 GO
 
 -- 2. Importar Unidades Funcionales (CSV)
-PRINT '2. Ejecutando sp_Importar_UF...';
-EXEC sp_Importar_UnidadesFuncionales 
+PRINT '2. Ejecutando dbo.sp_Importar_UF...';
+EXEC dbo.sp_Importar_UnidadesFuncionales 
     @RutaArchivoTXT = 'D:\Diego\Downloads\ARCHIVOS_TP_BDA\ARCHIVOS\UF por consorcio.txt';
 GO
 
 -- 3. Importar Personas (Inquilinos/Propietarios) (CSV)
-PRINT '3. Ejecutando sp_Importar_Personas...';
-EXEC sp_Importar_Personas 
+PRINT '3. Ejecutando dbo.sp_Importar_Personas...';
+EXEC dbo.sp_Importar_Personas 
     @RutaArchivoCSV = 'D:\Diego\Downloads\ARCHIVOS_TP_BDA\ARCHIVOS\Inquilino-propietarios-datos.csv';
 GO
 
 -- 4. Vincular Personas con Unidades Funcionales (CSV)
-PRINT '4. Ejecutando sp_Importar_UF_Persona...';
-EXEC sp_Importar_UF_Persona 
+PRINT '4. Ejecutando dbo.sp_Importar_UF_Persona...';
+EXEC dbo.sp_Importar_UF_Persona 
     @RutaArchivoCSV = 'D:\Diego\Downloads\ARCHIVOS_TP_BDA\ARCHIVOS\Inquilino-propietarios-UF.csv';
 GO
 
 -- 5. Importar Proveedores (Excel)
-PRINT '5. Ejecutando sp_Importar_Proveedores...';
-EXEC sp_Importar_Proveedores
+PRINT '5. Ejecutando dbo.sp_Importar_Proveedores...';
+EXEC dbo.sp_Importar_Proveedores
     @RutaArchivoXLSX = 'D:\Diego\Downloads\ARCHIVOS_TP_BDA\ARCHIVOS\datos varios.xlsx';
 GO
 
 -- 6. Importar Gastos (JSON)
-PRINT '6. Ejecutando sp_Importar_Gastos_JSON...';
-EXEC sp_Importar_Gastos_JSON 
+PRINT '6. Ejecutando dbo.sp_Importar_Gastos_JSON...';
+EXEC dbo.sp_Importar_Gastos_JSON 
     @RutaArchivoJSON = 'D:\Diego\Downloads\ARCHIVOS_TP_BDA\ARCHIVOS\Servicios.Servicios.json';
 GO
 
 -- 7. Importar y Conciliar Pagos (CSV)
 -- (Este SP asume que las Expensas ya fueron generadas)
-PRINT '7. Ejecutando sp_Importar_PagosConsorcios...';
-EXEC sp_Importar_PagosConsorcios 
+PRINT '7. Ejecutando dbo.sp_Importar_PagosConsorcios...';
+EXEC dbo.sp_Importar_PagosConsorcios 
     @RutaArchivoCSV = 'D:\Diego\Downloads\ARCHIVOS_TP_BDA\ARCHIVOS\pagos_consorcios.csv';
 GO
 
 PRINT '--- Proceso de Importacion Finalizado ---';
-GO
